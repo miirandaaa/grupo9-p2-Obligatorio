1a249ed1c21d55c71bcd3bdd0382fc7b1cd7a8ae
miirandaaa <131990078+miirandaaa@users.noreply.github.com>
2023-06-01 11:21:49 -0300

    getter

--- Obligatorio/src/User.java
+++ Obligatorio/src/User.java
@@ -1,4 +1,25 @@
 public class User {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
     private String name;
     private long id;
+
+    public User(String name, long id) {
+        this.name = name;
+        this.id = id;
+    }
 }
