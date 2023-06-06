a65af2416531d2c377e258824d87d92fa9852e88
Geronimo Garcia <gagarcia@correo.um.edu.uy>
2023-06-06 12:59:59 -0300

    Conf
    Conf

--- Obligatorio/src/CargaDatos.java
+++ Obligatorio/src/CargaDatos.java
@@ -1,3 +1,21 @@
 import org.apache.commons.csv.*;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
 public class CargaDatos {
+    public void datos() throws IOException {
+        Reader in = new FileReader("grupo9-p2-Obligatorio/Obligatorio/Dataset/f1_dataset_test.csv");
+        Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
+        int counter=100;
+        for (CSVRecord record : records) {
+            System.out.println(record.get(1));
+            counter--;
+            if(counter==0){
+                break;
+            }
+        }
+    }
 }
