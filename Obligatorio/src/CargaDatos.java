e257b9d7aae2cb79beb45031b18c6c59fadc7621
Geronimo Garcia <gagarcia@correo.um.edu.uy>
2023-06-07 10:05:47 -0300

    Config Inicial

--- Obligatorio/src/CargaDatos.java
+++ Obligatorio/src/CargaDatos.java
@@ -7,11 +7,12 @@ import java.io.Reader;
 
 public class CargaDatos {
     public void datos() throws IOException {
-        Reader in = new FileReader("grupo9-p2-Obligatorio/Obligatorio/Dataset/f1_dataset_test.csv");
+        Reader in = new FileReader("src/DataFile/f1_dataset_test.csv");
         Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
-        int counter=100;
+        int counter=10;
         for (CSVRecord record : records) {
-            System.out.println(record.get(1));
+            System.out.println(record.get(10));
+            System.out.println("============================================================");
             counter--;
             if(counter==0){
                 break;
