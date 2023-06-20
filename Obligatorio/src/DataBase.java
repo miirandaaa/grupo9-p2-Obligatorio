a5eebdec2f2f40c15255ffb89ecee00735c49c6d
miirandaaa <131990078+miirandaaa@users.noreply.github.com>
2023-06-20 13:11:54 -0300

    funciones

--- Obligatorio/src/DataBase.java
+++ Obligatorio/src/DataBase.java
@@ -1,4 +1,33 @@
 import uy.edu.um.prog2.adt.hash.HashTableImpl;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class DataBase {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        boolean datosCargados = false;
+        int opcion = 0;
+        while (opcion != 8) {
+            System.out.println("Menu principal: \n1 Cargar Datos \n2 Pilotos mas Mencionados \n3 Top 15 Usuarios con mas Tweets \n4 Cantidad de Hashtags \n5 Hashtag mas Usado \n6 Top 7 Cuentas con mas Favoritos \n7 Cantidad de Tweets con una Frase Especifica \n8 Salir \nOpcion:  ");
+            opcion = sc.nextInt();
+
+            if (opcion == 1 && !datosCargados){
+            CargaDatos carga = new CargaDatos();
+            carga.datos();
+            datosCargados = true;
+            }
+
+            if (opcion == 2 && datosCargados) {
+                System.out.println("Ingrese el año que desea consultar (2021 o 2022): ");
+                int anio = sc.nextInt();
+                System.out.println("Ingrese el mes que desea consultar (1 al 12): ");
+                int mes = sc.nextInt();
+                //Aca va la funcion que hay que crear sape
+                System.out.println("Los 10 Pilotos mas Mencionados son: ");
+            }
+
+            sc.close();
+        }
+    }
 }
--- Obligatorio/src/Main.java
+++ Obligatorio/src/Main.java
@@ -1,9 +1,27 @@
 import java.io.IOException;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         CargaDatos carga=new CargaDatos();
-
         carga.datos();
+        Scanner sc = new Scanner(System.in);
+        boolean datosCargados = false;
+        System.out.println("Menu principal: ");
     }
+
 }
+
+
+
+
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Hello, " + name + "! You are " + age + " years old.");
+
+        scanner.close();
+    }
